efe42ab9d641bf1d5d55cf348fbb69383fb3d05a
muthusenthilkumar <46280655+muthusenthilkumar@users.noreply.github.com>
2019-01-07 19:18:18 +0530

    Code changes merged

---
+++ HIClaims/BL/CustomerClaim.cs
@@ -0,0 +1,46 @@
+﻿using HIClaims.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace HIClaims.BL
+{
+    public class CustomerClaim
+    {
+        public List<Claim> GetClaims()
+        {
+            string result = string.Empty;
+            var claims = new List<Claim>();
+            
+            var resourcePath = HttpRuntime.AppDomainAppPath+ "/Resources/ClaimData.json";
+            using (StreamReader reader = new StreamReader(resourcePath))
+            {
+                result = reader.ReadToEnd();
+            }
+            claims = JsonConvert.DeserializeObject<List<Claim>>(result);
+            return claims;
+        }
+
+        public bool SaveClaims( Claim claim)
+        {
+            try
+            {
+                var resourcePath = HttpRuntime.AppDomainAppPath + "/Resources/ClaimData.json";
+
+                var claims = GetClaims();
+                claims.Add(claim);
+                var result = JsonConvert.SerializeObject(claims);
+                System.IO.File.WriteAllText(resourcePath, result);
+                return true;
+            }catch(Exception e)
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ HIClaims/Models/Claim.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HIClaims.Models
+{
+    public class Claim
+    {
+        public int ClaimNo { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime ClaimedDate { get; set; }
+        public float ClaimAmount { get; set; }
+        public int PolicyNo { get; set;}
+        public string Gender { get; set; }
+
+    }
+}
